1ef6183defeb87bb7378dcb3f4bd362cd2ce01f3
Angela Ebirim <cebirim@gmail.com>
2014-01-14 22:42:21 +0000

    rentify app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rentify::Application.config.secret_key_base = 'b73bcd1c7fb010b540ff5898824c11a53e1bb3eb39a51827f08a05732d237baec1a8eefde3ed4d36cf8056b459ef47b84243b9ce1dbe61b30257e6eb717f90ca'
